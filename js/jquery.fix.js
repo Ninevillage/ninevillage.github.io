b2b7b16796e93e6958221b124aff8d18b790f6ed
Nax (aka Matt) <m.neundorf@live.de>
2015-04-14 19:05:44 +0000

    move string prototype to jquery fix, dont change ref thumbnail height

--- js/jquery.fix.js
+++ js/jquery.fix.js
@@ -3,3 +3,8 @@ jQuery.browser.mozilla = /mozilla/.test(navigator.userAgent.toLowerCase()) && !/
 jQuery.browser.webkit = /webkit/.test(navigator.userAgent.toLowerCase());
 jQuery.browser.opera = /opera/.test(navigator.userAgent.toLowerCase());
 jQuery.browser.msie = /msie/.test(navigator.userAgent.toLowerCase());
+
+
+String.prototype.capitalizeFirstLetter = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+};
